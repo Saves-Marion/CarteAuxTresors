d9bd6b95bbf0efaf4a8b9e1c8cb0417914dc5a73
Saves Marion <74530367+Saves-Marion@users.noreply.github.com>
2022-07-22 14:24:46 +0200

    modification inputPath and outputPath

--- src/main/java/configuration/FileNameConfiguration.java
+++ src/main/java/configuration/FileNameConfiguration.java
@@ -1,8 +1,8 @@
 package main.java.configuration;
 
 public class FileNameConfiguration {
-    public static final String fileInputName = "src/main/ressources/fileWithComment.txt";
-    public static final String fileOutputName = "src/main/ressources/resultatsWithoutComment.txt";
+    public static final String fileInputName = "src/main/ressources/file.txt";
+    public static final String fileOutputName = "src/main/ressources/resultats.txt";
 
     /*
     public static String fileInputName = "src/main/ressources/file.txt";
