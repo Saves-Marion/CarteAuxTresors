0f2245530219dbfcb9f3a36e6c923be7de1d4c68
Saves Marion <74530367+Saves-Marion@users.noreply.github.com>
2022-07-22 11:53:17 +0200

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import main.java.implementation.TreasureMap;
+
+public class Main {
+
+    public static void main(String args[]) throws Exception {
+        TreasureMap.runTreasureMap();
+    }
+}
